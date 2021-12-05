a8b4a84582862790b9e1f2201c8b9f4e0f1ef19c
aoirint <aoirint@gmail.com>
2021-12-06 04:43:30 +0900

    morphing

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 from typing import Any, Dict
 from urllib.parse import urljoin
 
-def audio_query(engine_url: str, speaker: int, text: str) -> Dict[str, Any]:
+def audio_query(engine_url: str, text: str, speaker: int) -> Dict[str, Any]:
     headers = {
         'Content-Type': 'application/json',
     }
@@ -20,7 +20,7 @@ def audio_query(engine_url: str, speaker: int, text: str) -> Dict[str, Any]:
 
     return r.json()
 
-def synthesis(engine_url: str, speaker: int, query: Dict[str, Any]) -> bytes:
+def synthesis(engine_url: str, query: Dict[str, Any], speaker: int) -> bytes:
     headers = {
         'Content-Type': 'application/json',
         'Accept': 'audio/wav',
@@ -37,6 +37,25 @@ def synthesis(engine_url: str, speaker: int, query: Dict[str, Any]) -> bytes:
 
     return r.content
 
+def synthesis_morphing(engine_url: str, query: Dict[str, Any], base_speaker: int, target_speaker: int, morph_rate: float) -> bytes:
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'audio/wav',
+    }
+
+    params = {
+        'base_speaker': base_speaker,
+        'target_speaker': target_speaker,
+        'morph_rate': morph_rate,
+    }
+
+    r = requests.post(urljoin(engine_url, 'synthesis_morphing'), headers=headers, params=params, data=json.dumps(query).encode('utf-8'))
+
+    if r.status_code != 200:
+        raise Exception(r.text)
+
+    return r.content
+
 
 if __name__ == '__main__':
     from datetime import datetime as dt
@@ -47,19 +66,49 @@ if __name__ == '__main__':
     import os
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('speaker', type=int)
-    parser.add_argument('text', type=str)
-    parser.add_argument('--engine-url', type=str, default=os.environ.get('VOICEVOX_ENGINE_URL', 'http://127.0.0.1:50021'))
+    subparsers = parser.add_subparsers(dest='subparser_name')
+
+    parser_synthesis = subparsers.add_parser('synthesis')
+    parser_synthesis.add_argument('-t', '--text', type=str, required=True)
+    parser_synthesis.add_argument('-s', '--speaker', type=int, required=True)
+    parser_synthesis.add_argument('--engine-url', type=str, default=os.environ.get('VOICEVOX_ENGINE_URL', 'http://127.0.0.1:50021'))
+
+    parser_morphing = subparsers.add_parser('morphing')
+    parser_morphing.add_argument('-t', '--text', type=str, required=True)
+    parser_morphing.add_argument('-s', '--base-speaker', type=int, required=True)
+    parser_morphing.add_argument('-ts', '--target-speaker', type=int, required=True)
+    parser_morphing.add_argument('-mr', '--morph-rate', type=float, required=True)
+    parser_morphing.add_argument('--engine-url', type=str, default=os.environ.get('VOICEVOX_ENGINE_URL', 'http://127.0.0.1:50021'))
+
     args = parser.parse_args()
 
-    speaker = args.speaker
-    text = args.text
-    engine_url = args.engine_url
-    filename = dt.now().strftime('%Y%m%d_%H%M%S')
+    if args.subparser_name == 'synthesis':
+        text = args.text
+        speaker = args.speaker
+        engine_url = args.engine_url
+        filename = dt.now().strftime('%Y%m%d_%H%M%S')
 
-    query = audio_query(engine_url, speaker, text)
+        query = audio_query(engine_url, text, speaker)
 
-    binary = synthesis(engine_url, speaker, query)
+        binary = synthesis(engine_url, query, speaker)
 
-    with open(f'{filename}.wav', 'wb') as fp:
-        fp.write(binary)
+        with open(f'{filename}.wav', 'wb') as fp:
+            fp.write(binary)
+
+    elif args.subparser_name == 'morphing':
+        text = args.text
+        base_speaker = args.base_speaker
+        target_speaker = args.target_speaker
+        morph_rate = args.morph_rate
+        engine_url = args.engine_url
+        filename = dt.now().strftime('%Y%m%d_%H%M%S')
+
+        query = audio_query(engine_url, text, base_speaker)
+
+        binary = synthesis_morphing(engine_url, query, base_speaker, target_speaker, morph_rate)
+
+        with open(f'{filename}.wav', 'wb') as fp:
+            fp.write(binary)
+
+    else:
+        parser.print_help()
